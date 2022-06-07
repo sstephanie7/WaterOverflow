8a492df1a22548970f96672fe8bf6e819f5d88d5
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-07 16:08:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CAPACITY = 255;
+            int nLines = int.Parse(Console.ReadLine());
+            int totalQuantity = CAPACITY;
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int currentQuantity = int.Parse(Console.ReadLine()); 
+
+                if(totalQuantity - currentQuantity >= 0)
+                {
+                    totalQuantity -= currentQuantity;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            int totalFilledQuantity = CAPACITY - totalQuantity;
+            Console.WriteLine(totalFilledQuantity);
+        }
+    }
+}
